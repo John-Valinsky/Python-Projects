5fa8ade1b9098b5dc3ca28505d541e1d5698b171
John Lalremruata <johnvalinsky07@gmail.com>
2026-02-20 00:57:11 +0530

    .

--- Windows/Windows Search/WsCr.py
+++ Windows/Windows Search/WsCr.py
@@ -27,3 +27,15 @@ def get_available_drives():
             drives.append(f"{letter}:\\        |")
         bitmask >>= 1
     return drives
+
+def save_output_to_file(output, filename):
+    """Saves the output to a text file without ANSI escape codes."""
+    try:
+        # Remove ANSI escape codes from the output
+        cleaned_output = re.sub(r'\033\[[0-9;]*m', '', output)
+
+        with open(filename, "w") as file:
+            file.write(cleaned_output)
+        print(f"Output has been saved to {filename}")
+    except Exception as e:
+        print(f"An error occurred while saving the file: {e}")
