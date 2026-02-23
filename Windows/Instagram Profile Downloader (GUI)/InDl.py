98a76f72e5e3661807cd5066462e047acb011af1
John Lalremruata <johnvalinsky07@gmail.com>
2026-02-24 00:21:21 +0530

    .

--- Windows/Instagram Profile Downloader (GUI)/InDl.py
+++ Windows/Instagram Profile Downloader (GUI)/InDl.py
@@ -2,3 +2,10 @@ import subprocess
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
+def center_window(win, width, height):
+    win.update_idletasks()
+    screen_width = win.winfo_screenwidth()
+    screen_height = win.winfo_screenheight()
+    x = (screen_width // 2) - (width // 2)
+    y = (screen_height // 2) - (height // 2)
+    win.geometry(f"{width}x{height}+{x}+{y}")
