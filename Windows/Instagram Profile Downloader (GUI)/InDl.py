53c334d53af8fd54efb7137196f6c6344d8e060c
John Lalremruata <johnvalinsky07@gmail.com>
2026-02-24 00:21:51 +0530

    .

--- Windows/Instagram Profile Downloader (GUI)/InDl.py
+++ Windows/Instagram Profile Downloader (GUI)/InDl.py
@@ -9,3 +9,9 @@ def center_window(win, width, height):
     x = (screen_width // 2) - (width // 2)
     y = (screen_height // 2) - (height // 2)
     win.geometry(f"{width}x{height}+{x}+{y}")
+
+def browse_folder():
+    folder = filedialog.askdirectory(title="Select folder to save downloads")
+    if folder:
+        folder_var.set(folder)
+
